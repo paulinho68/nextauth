dfddb934447220a38f57142a55eff4f219cd54e3
Paulo Vitor <paulovitor2123@gmail.com>
2021-08-21 01:17:34 -0300

    add authentication and save user data with nookies in cookies

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,70 @@
+import { useRouter } from 'next/dist/client/router';
+import { createContext, ReactNode, useState } from 'react';
+import { api } from '../services/api';
+import { setCookie } from 'nookies';
+
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
+type SignInCredentials = {
+    email: string;
+    password: string;
+}
+
+type AuthContextData = {
+    signIn(creadentials: SignInCredentials): Promise<void>;
+    isAuthenticated: boolean;
+    user: User;
+}
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+    const router = useRouter();
+    const [user, setUser] = useState<User>(null);
+    const isAuthenticated = !!user;
+
+    async function signIn({ email, password }: SignInCredentials) {
+        try {
+            const response = await api.post('/sessions', {
+                email,
+                password
+            });
+
+            const { token, refreshToken, permissions, roles } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            });
+
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            });
+
+            setUser({
+                email,
+                permissions,
+                roles
+            });
+            router.push('/dashboard');
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
